81671495023f62caf223d40e503496d1191f83c9
Mohamed Salat <89856630+JustGetAClass@users.noreply.github.com>
2023-02-26 12:06:27 +0300

    express get, post and listen working

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
-const https = require("https");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 const port = 3000;
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+	res.sendFile(`${__dirname}/signup.html`);
+});
+
+app.post("/", (req, res) => {
+    let firstName = req.body.fname;
+    let lastName = req.body.lname;
+    let email = req.body.email;
+    console.log(firstName, lastName, email);
+})
 
 app.listen(port, () => {
-    console.log(`listening at port ${port}`)
-})
+	console.log(`listening at port ${port}`);
+});
